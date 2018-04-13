9afdc4246e754ea5bf5e332fefd618bb76308950
prijatelj <theomnipotentwoof@gmail.com>
2018-04-13 02:16:14 -0400

    refactored intelligent agent code a bit.

---
+++ src/intelligent_agent/decision_tree_static.py
@@ -0,0 +1,91 @@
+"""
+A static decision tree implementation of an intelligent agent for holding a
+conversation. In otherwords, the baseline rule system of the intelligent agent.
+
+based on hierarch:
+    new convo / or on-going
+        change topic / stay on topic
+            passive / assertive tactic
+
+:author: Derek S. Prijatelj
+"""
+
+from intelligent_agent import tactic
+
+def decision_tree_static(conversation_history, chatbot, user, personas=None):
+    """
+    A manual decision tree to implement a rule based system as a baseline.
+
+    :param conversation_history: current ConversationHistory
+    :param chatbot: Persona of chatbot
+    :param user: Persona of user
+    :param personas: Dictionary of str "persona_id" to Persona
+    """
+    # Assess mood and magnitude of change to mood necessary
+    mood_magnitude = chatbot.personality.mood - user.personality.mood
+
+    # Assess topic sentiment in relation to desired mood
+    topic_magnitude = user.topic_magnitude(
+        last_utterance.topic,
+        chatbot.personality.mood
+    )
+
+    # static reactions:
+    if last_utterance.dialogue_act == DialogueAct.farewell:
+        return Utterance(
+            chatbot_id,
+            DialogueAct.farewell,
+            "self_user",
+            chatbot.personality.mood,
+            chatbot.personality.assertiveness
+        )
+
+    # TODO add ability to reference previous conversations for returning users
+    if len(conversation_history.topic_to_utterance.keys()) <= 1:
+        # New conversation started
+        tactic.query()
+
+        # prev = greeting, then greeting, query, etc.
+        # prev = question, then answer
+        # statement then ... idk response? who are you?
+
+    else: # Ongoing Conversation
+        # topics have been discussed, and conversation ongoing
+        # go through topics of desired sentiment till exhausted.
+        # query new topics if topics of desired sentiment are exhausted.
+        #TODO Need a way to know what topics have been exhausted! ia only. is it ia only?
+
+        if change_topic(mood_magnitude, topic_magnitude,
+                chatbot.personality.mood):
+            if respond_passive(chatbot, user, mood_magnitude, topic_magnitude):
+
+            else:
+
+        else:
+
+    return
+
+def psych():
+    # Placeholder for now, will put w/in hierarchy where necessary.
+    #TODO psychiatrist for self_user only, not self_bot.
+    if topic_is_self(last_utterance.topic) \
+            or topic_is_user(last_utterance.topic):
+        return tactics.psychiatrist(last_utterance, chatbot_id)
+
+def change_topic(mood_magnitude, topic_magnitude, desired_mood):
+    return
+
+def respond_passively(chatbot, user, mood_magnitude, topic_magnitude):
+    return
+
+def change_topic_passive():
+    return
+
+def change_topic_assertive():
+    return
+
+def stay_on_topic_passive():
+    return
+
+def stay_on_topic_assertive():
+    return
--- src/intelligent_agent/intelligent_agent.py
+++ src/intelligent_agent/intelligent_agent.py
@@ -9,7 +9,6 @@ methods, and optimization methods.
 :author: Derek S. Prijatelj
 """
 
-from intelligent_agent import tactics
 from conversation import DialogueAct, Utterance, \
     is_statement, is_question, is_response_action, is_backchannel, \
     topic_is_self, topic_is_user
@@ -26,81 +25,46 @@ _standard_topic = [
 ]
 
 #def decide_response(simulation, user, conversation_history):
-def decide_response(conversation_history, speaker_id, persona_dict):
+def decide_response(conversation_history, chatbot_id, persona_dict):
     """
     Main interface for intelligent agent to decide how to response. With the NLU
     information and the persona making this decision, generates the metadata of
     the persona's response utterance. This metadata matches the type and format
     of the NLU information.
     """
-    speaker = persona_dict[speaker_id]
+    chatbot = persona_dict[chatbot_id]
     participants = conversation_history.participants
-    participants.remove(speaker_id)
+    participants.remove(chatbot_id)
     user = persona_dict[participants[0]]
     last_utterance = conversation_history.last_utterance
 
     # Assess mood and magnitude of change to mood necessary
-    mood_magnitude = speaker.personality.mood - user.personality.mood
+    mood_magnitude = chatbot.personality.mood - user.personality.mood
 
     # Assess topic sentiment in relation to desired mood
-    topic_magnitude = speaker.topic_magnitude(
+    topic_magnitude = chatbot.topic_magnitude(
         last_utterance.topic,
-        speaker.personality.mood
+        chatbot.personality.mood
     )
     # TODO give first utterance, should never occur in prototype
 
     # static reactions:
     if last_utterance.dialogue_act == DialogueAct.farewell:
         return Utterance(
-            speaker_id,
+            chatbot_id,
             DialogueAct.farewell,
             "self_user",
-            speaker.personality.mood,
-            speaker.personality.assertiveness
+            chatbot.personality.mood,
+            chatbot.personality.assertiveness
         )
-    elif last_utterance.dialogue_act == DialogueAct.greeting:
-        return Utterance(
-            speaker_id,
-            DialogueAct.greeting,
-            "self_user",
-            speaker.personality.mood,
-            speaker.personality.assertiveness
-        )
+    #elif last_utterance.dialogue_act == DialogueAct.greeting:
+    #    return Utterance(
+    #        chatbot_id,
+    #        DialogueAct.greeting,
+    #        "self_user",
+    #        chatbot.personality.mood,
+    #        chatbot.personality.assertiveness
+    #    )
 
-    # TODO add ability to reference previous conversations for returning users
-    if len(conversation_history.topic_to_utterance.keys()) == 0:
-        # New conversation started
-        query()
-
-        # prev = greeting, then greeting, query, etc.
-        # prev = question, then answer
-        # statement then ... idk response? who are you?
-
-    else:
-        # topics have been discussed, and conversation ongoing
-        # go through topics of desired sentiment till exhausted.
-        # query new topics if topics of desired sentiment are exhausted.
-        #TODO Need a way to know what topics have been exhausted! ia only. is it ia only?
-
-
-        #TODO psychiatrist for self_user only, not self_bot.
-        if topic_is_self(last_utterance.topic) \
-                or topic_is_user(last_utterance.topic):
-            return tactics.psychiatrist(last_utterance, speaker_id)
-        elif last_utterance.topic in _standard_topic:
-            # Check if standard topic:
-            utterance_metadata = handle_standard_topic(simulation,
-                user,
-                conversation_history,
-                mood_magnitude
-            )
-
-        # TODO remove this, this is just to stop code from crashing until IA finished
-        return Utterance(speaker_id, DialogueAct.other, "None", 5, 5)
-
-def decision_tree_static():
-    return
-
-def handle_standard_topic(conversation_history, speaker, user, mood_magnitude):
-    """ Handles responding to standard topics, such as weather and news.  """
-    return
+    # TODO remove this, this is just to stop code from crashing until IA finished
+    return Utterance(chatbot_id, DialogueAct.other, "None", 5, 5)
