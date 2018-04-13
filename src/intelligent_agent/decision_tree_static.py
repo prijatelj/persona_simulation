b573268edf0ac65f0a22889047acd73c6f694135
prijatelj <theomnipotentwoof@gmail.com>
2018-04-13 08:45:19 -0400

    blocked out the main parts in comments for the 4 main response types in ongoing conversations.

--- src/intelligent_agent/decision_tree_static.py
+++ src/intelligent_agent/decision_tree_static.py
@@ -12,12 +12,12 @@ based on hierarch:
 
 from intelligent_agent import tactic
 
-def decision_tree_static(conversation_history, chatbot, user, personas=None):
+def decision_tree_static(conversation, chatbot, user, personas=None):
     """
     A manually constructed decision tree to implement a rule based system as a
     baseline.
 
-    :param conversation_history: current ConversationHistory
+    :param conversation: current Conversation
     :param chatbot: Persona of chatbot
     :param user: Persona of user
     :param personas: Dictionary of str "persona_id" to Persona
@@ -42,7 +42,7 @@ def decision_tree_static(conversation_history, chatbot, user, personas=None):
         )
 
     # TODO add ability to reference previous conversations for returning users
-    if len(conversation_history.topic_to_utterance.keys()) <= 1:
+    if len(conversation.topic_to_utterance.keys()) <= 1:
         # New conversation started
         tactic.query()
 
@@ -56,37 +56,93 @@ def decision_tree_static(conversation_history, chatbot, user, personas=None):
         # query new topics if topics of desired sentiment are exhausted.
         #TODO Need a way to know what topics have been exhausted! ia only. is it ia only?
 
-        #if change_topic(mood_magnitude, topic_magnitude,
-        #        chatbot.personality.mood):
-        #   if respond_passive(chatbot, user, mood_magnitude, topic_magnitude):
-        #
-        #   else:
-        #
-        #else:
-        return
-    return
+        # static response:
+        # if greeting in middle of conversation either ignore or question it.
+        #   perhaps respond confused.
+
+
+
+        if change_topic(mood_magnitude, topic_magnitude,
+                chatbot.personality.mood):
+            if respond_passively(chatbot, user, mood_magnitude):
+                return change_topic_passive(conversation, chatbot, user,
+                    mood_magnitude, topic_magnitude, personas)
+            else: # respond assertively
+                return change_topic_assertive(conversation, chatbot, user,
+                    mood_magnitude, topic_magnitude, personas)
+
+        else: # Stay on topic
+            if respond_passively(chatbot, user, mood_magnitude):
+                return stay_on_topic_passive(conversation, chatbot, user,
+                    mood_magnitude, topic_magnitude, personas)
+            else: # respond assertively
+                return stay_on_topic_assertive(conversation, chatbot, user,
+                    mood_magnitude, topic_magnitude, personas)
 
 def psych():
     # Placeholder for now, will put w/in hierarchy where necessary.
     #TODO psychiatrist for self_user only, not self_bot.
+    # only really fits when stay on topic. Maybe if change topic to user too?
     if topic_is_self(last_utterance.topic) \
             or topic_is_user(last_utterance.topic):
         return tactics.psychiatrist(last_utterance, chatbot_id)
 
-def change_topic(mood_magnitude, topic_magnitude, desired_mood):
+def change_topic(mood_magnitude, topic_magnitude, chatbot):
+    # if topic magnitude is far from desired, (and is mood_magnitude) change
+    # TODO can calculate how much the desire to change is based on assertiveness
+    if chatbot.personality.assertiveness > 5:
+        return abs(topic_magnitude) >= 2 and abs(mood_magnitude) >= 2)
+    else:
+        return abs(topic_magnitude) >= 3 and abs(mood_magnitude) >= 3)
+
+def respond_passively(chatbot, user, mood_magnitude):
+    # TODO can calculate how much desire to be assertive based on chatbot & user
+    # ie. can play off user's own assertiveness to complement it.
+    # TODO mood may be affected by chatbot's assertiveness, esp. if deemed rude.
+    return chatbot.personality.assertiveness > 5
+
+
+# TODO have random select, have it be a skewed normal, and mean wherever
+# personality.mood is.
+def change_topic_passive(conversation, chatbot, user, mood_magnitude,
+        topic_magnitude, personas):
+
+    # silence or no response or minimal response
+
+    # ask to change topic, no expression of sent, not alternatives
+
+    # express sent, no alternatives
+
     return
 
-def respond_passively(chatbot, user, mood_magnitude, topic_magnitude):
+def change_topic_assertive(conversation, chatbot, user, mood_magnitude,
+        topic_magnitude, personas):
+
+    # provide alts. only
+
+    # express sent, provide alts.
+
+    # provide alts & begin talking
+
+    # begin talking (abruptly change topic)
+
     return
 
-def change_topic_passive():
+def stay_on_topic_passive(conversation, chatbot, user, mood_magnitude,
+        topic_magnitude, personas):
+
+    # Listening oriented
+
+    # Reinforce passively, "me too", "interesting."
+
+
     return
 
-def change_topic_assertive():
-    return
+def stay_on_topic_assertive(conversation, chatbot, user, mood_magnitude,
+        topic_magnitude, personas):
 
-def stay_on_topic_passive():
-    return
+    # Leading Conversation, Informing/Expressing views
 
-def stay_on_topic_assertive():
+    # Reinforce actively "yes and this too..."
+
     return
